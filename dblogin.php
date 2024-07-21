ac115fc734c2352bdaffdc628d028b359d026bc4
azaryao <127715182+azaryao@users.noreply.github.com>
2024-07-21 21:52:33 +0700

    Update Website-DepKes
    login page, form page, and database

--- dblogin.php
+++ dblogin.php
@@ -1,1 +1,14 @@
-<!-- Sedang dikerjakan -->
+<?php
+ include 'connect.php';
+
+ $username = $_POST['username'];
+ $password = $_POST['password'];
+
+ $stmt = $conn->prepare("insert into patient_login(username, password)
+ values(?, ?)");
+ $stmt->bind_param("ss", $username, $password);
+ $stmt->execute();
+ echo "Successfully login";
+ $stmt->close();
+ $conn->close();
+?>
--- dbpatient.php
+++ dbpatient.php
@@ -1,1 +1,18 @@
-<!-- Sedang dikerjakan -->
+<?php
+ include 'connect.php';
+
+ $weight = $_POST['weight'];
+ $height = $_POST['height'];
+ $bloodPressure = $_POST['bloodPressure'];
+ $bloodSugar = $_POST['bloodSugar'];
+ $cholesterol = $_POST['cholesterol'];
+ $gout = $_POST['gout'];
+
+ $stmt = $conn->prepare("insert into patient_data(weight, height, bloodPressure, bloodSugar, cholesterol, gout)
+ values(?, ?, ?, ?, ?, ?)");
+ $stmt->bind_param("iiiiii", $weight, $height, $bloodPressure, $bloodSugar, $cholesterol, $gout);
+ $stmt->execute();
+ echo "Successfully submit patient data";
+ $stmt->close();
+ $conn->close();
+?>
